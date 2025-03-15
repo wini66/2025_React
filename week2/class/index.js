6d12275e6ccf9e336e3117b0ae36867b842a5db1
wini66 <119557644+wini66@users.noreply.github.com>
2025-03-16 02:01:20 +0900

    Add files via upload

---
+++ week2/class/index.js
@@ -0,0 +1,23 @@
+import './index.css';
+import App from './App';
+import Library from './chapter_03/Library';
+//   수정: MyApp -> App
+import reportWebVitals from './reportWebVitals';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Library />  {/* 수정: MyApp -> App */}
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
