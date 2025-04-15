d62ab67cfc3ae91e01e2d068eddad85c00bed6fc
wini66 <119557644+wini66@users.noreply.github.com>
2025-04-15 10:03:09 +0900

    Add files via upload

---
+++ week7/class/Timer.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+
+function Timer() {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("1초마다 실행");
+    }, 1000);
+    return () => {
+      console.log("타이머 정리");
+      clearInterval(interval);
+    };
+  }, []);
+  return <h1>타이머 실행 중...</h1>;
+}
+export default Timer;
