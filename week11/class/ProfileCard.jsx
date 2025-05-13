501dd76cd4bd0a401c3bf82b0cfa7e0b4138cfdc
wini66 <119557644+wini66@users.noreply.github.com>
2025-05-13 10:23:48 +0900

    Add files via upload

---
+++ week11/class/ProfileCard.jsx
@@ -0,0 +1,11 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+  return (
+    <Card title="InjeLee" backgroundColor="#4ea04e">
+      <p>안녕하세요, 소플입니다.</p>
+      <p>저는리액트를사용해서개발하고있습니다.</p>
+    </Card>
+  );
+}
+export default ProfileCard;
